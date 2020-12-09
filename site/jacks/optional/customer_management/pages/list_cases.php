8671aa5ef673ccf789ac8177520d7d61d3453b31
sk_arnov <obi.prog@gmail.com>
2020-12-09 19:16:45 +0600

    Case Report Fix

--- site/jacks/optional/customer_management/pages/list_cases.php
+++ site/jacks/optional/customer_management/pages/list_cases.php
@@ -1,4 +1,5 @@
 <?php
+
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Box\Spout\Common\Entity\Row;
 use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
@@ -91,44 +92,125 @@ if ($filter_entry_end_date)
     $filterString[] = 'End Date: ' . $filter_entry_end_date;
 
 if ($_GET['download_excel']) {
-    unset($args['select_fields']);
+    $args = array(
+        'select_fields' => array(
+            'pk_immediate_support_id' => 'dev_immediate_supports.pk_immediate_support_id',
+            'fk_staff_id' => 'dev_immediate_supports.fk_staff_id',
+            'fk_customer_id' => 'dev_immediate_supports.fk_customer_id',
+            'immediate_support' => 'dev_immediate_supports.immediate_support',
+            'entry_date' => 'dev_immediate_supports.entry_date AS entry_date',
+            'arrival_place' => 'dev_immediate_supports.arrival_place',
+            'plan_date' => 'dev_reintegration_plan.plan_date',
+            'reintegration_financial_service' => 'dev_reintegration_plan.reintegration_financial_service',
+            'service_requested' => 'dev_reintegration_plan.service_requested',
+            'other_service_requested' => 'dev_reintegration_plan.other_service_requested',
+            'social_protection' => 'dev_reintegration_plan.social_protection',
+            'security_measure' => 'dev_reintegration_plan.security_measure',
+            'service_requested_note' => 'dev_reintegration_plan.service_requested_note',
+            'first_meeting' => 'dev_psycho_supports.first_meeting',
+            'problem_identified' => 'dev_psycho_supports.problem_identified',
+            'problem_description' => 'dev_psycho_supports.problem_description',
+            'initial_plan' => 'dev_psycho_supports.initial_plan',
+            'family_counseling' => 'dev_psycho_supports.family_counseling',
+            'session_place' => 'dev_psycho_supports.session_place',
+            'session_number' => 'dev_psycho_supports.session_number',
+            'session_duration' => 'dev_psycho_supports.session_duration',
+            'other_requirements' => 'dev_psycho_supports.other_requirements',
+            'reffer_to' => 'dev_psycho_supports.reffer_to',
+            'referr_address' => 'dev_psycho_supports.referr_address',
+            'contact_number' => 'dev_psycho_supports.contact_number',
+            'reason_for_reffer' => 'dev_psycho_supports.reason_for_reffer',
+            'other_reason_for_reffer' => 'dev_psycho_supports.other_reason_for_reffer',
+            'full_name' => 'dev_customers.full_name',
+            'inkind_project' => 'dev_economic_supports.inkind_project',
+            'other_inkind_project' => 'dev_economic_supports.other_inkind_project',
+            'entry_date' => 'dev_economic_supports.entry_date AS economic_reintegration_date',
+            'is_certification_received' => 'dev_economic_supports.is_certification_received',
+            'training_used' => 'dev_economic_supports.training_used',
+            'economic_other_comments' => 'dev_economic_supports.other_comments AS economic_other_comments',
+            'microbusiness_established' => 'dev_economic_supports.microbusiness_established',
+            'month_inauguration' => 'dev_economic_supports.month_inauguration',
+            'year_inauguration' => 'dev_economic_supports.year_inauguration',
+            'family_training' => 'dev_economic_supports.family_training',
+            'traning_entry_date' => 'dev_economic_supports.traning_entry_date',
+            'place_traning' => 'dev_economic_supports.place_traning',
+            'duration_traning' => 'dev_economic_supports.duration_traning',
+            'training_status' => 'dev_economic_supports.training_status',
+            'financial_literacy_date' => 'dev_economic_supports.financial_literacy_date',
+            'business_development_date' => 'dev_economic_supports.business_development_date',
+            'product_development_date' => 'dev_economic_supports.product_development_date',
+            'entrepreneur_training_date' => 'dev_economic_supports.entrepreneur_training_date',
+            'other_financial_training_name' => 'dev_economic_supports.other_financial_training_name',
+            'other_financial_training_date' => 'dev_economic_supports.other_financial_training_date',
+            'entry_date' => 'dev_economic_reintegration_referrals.entry_date AS economic_reintegration_referral_date',
+            'is_vocational_training' => 'dev_economic_reintegration_referrals.is_vocational_training',
+            'received_vocational_training' => 'dev_economic_reintegration_referrals.received_vocational_training',
+            'other_received_vocational_training' => 'dev_economic_reintegration_referrals.other_received_vocational_training',
+            'received_vocational' => 'dev_economic_reintegration_referrals.received_vocational',
+            'other_received_vocational' => 'dev_economic_reintegration_referrals.other_received_vocational',
+            'economic_referrals_other_comments' => 'dev_economic_reintegration_referrals.other_comments AS economic_referrals_other_comments',
+            'is_economic_services' => 'dev_economic_reintegration_referrals.is_economic_services',
+            'economic_financial_service' => 'dev_economic_reintegration_referrals.economic_financial_service',
+            'economic_support' => 'dev_economic_reintegration_referrals.economic_support',
+            'other_economic_support' => 'dev_economic_reintegration_referrals.other_economic_support',
+            'is_assistance_received' => 'dev_economic_reintegration_referrals.is_assistance_received',
+            'refferd_to' => 'dev_economic_reintegration_referrals.refferd_to',
+            'refferd_address' => 'dev_economic_reintegration_referrals.refferd_address',
+            'trianing_date' => 'dev_economic_reintegration_referrals.trianing_date',
+            'place_of_training' => 'dev_economic_reintegration_referrals.place_of_training',
+            'duration_training' => 'dev_economic_reintegration_referrals.duration_training',
+            'status_traning' => 'dev_economic_reintegration_referrals.status_traning',
+            'assistance_utilized' => 'dev_economic_reintegration_referrals.assistance_utilized',
+            'job_placement_date' => 'dev_economic_reintegration_referrals.job_placement_date',
+            'financial_services_date' => 'dev_economic_reintegration_referrals.financial_services_date',
+            'reintegration_economic' => 'dev_social_supports.reintegration_economic',
+            'other_reintegration_economic' => 'dev_social_supports.other_reintegration_economic',
+            'soical_date' => 'dev_social_supports.soical_date',
+            'medical_date' => 'dev_social_supports.medical_date',
+            'date_education' => 'dev_social_supports.date_education',
+            'date_housing' => 'dev_social_supports.date_housing',
+            'date_legal' => 'dev_social_supports.date_legal',
+            'support_referred' => 'dev_social_supports.support_referred',
+            'other_support_referred' => 'dev_social_supports.other_support_referred',
+        ),
+    );
+
     unset($args['limit']);
-    
+
     $args['data_only'] = true;
     $data = $this->get_cases($args);
     $data = $data['data'];
 
     // This will be here in our project
 
-    $writer =WriterEntityFactory::createXLSXWriter();
+    $writer = WriterEntityFactory::createXLSXWriter();
     $style = (new StyleBuilder())
-           ->setFontBold()
-           ->setFontSize(12)
-           //->setShouldWrapText()
-           ->build();
+            ->setFontBold()
+            ->setFontSize(12)
+            //->setShouldWrapText()
+            ->build();
 
     $fileName = 'case-management-' . time() . '.xlsx';
     $writer->openToBrowser($fileName); // stream data directly to the browser
-
     // Header text
     $style2 = (new StyleBuilder())
-           ->setFontBold()
-           ->setFontSize(15)
-           //->setFontColor(Color::BLUE)
-           ->setShouldWrapText()
-           ->setCellAlignment(CellAlignment::LEFT)
-           ->build();
+            ->setFontBold()
+            ->setFontSize(15)
+            //->setFontColor(Color::BLUE)
+            ->setShouldWrapText()
+            ->setCellAlignment(CellAlignment::LEFT)
+            ->build();
 
     /** add a row at a time */
     $report_head = ['Case Management Report '];
-    $singleRow = WriterEntityFactory::createRowFromArray($report_head,$style2);
+    $singleRow = WriterEntityFactory::createRowFromArray($report_head, $style2);
     $writer->addRow($singleRow);
 
-    $report_date = ['Date: '.Date('d-m-Y H:i')];
+    $report_date = ['Date: ' . Date('d-m-Y H:i')];
     $reportDateRow = WriterEntityFactory::createRowFromArray($report_date);
     $writer->addRow($reportDateRow);
 
-    $filtered_with = ['Participant ID = '.$filter_id.', Name = '.$filter_name.', NID = '.$filter_nid.', Passport = '.$filter_passport.', Division = ' . $filter_division . ', District = ' . $filter_district . ', Sub-District = ' . $filter_sub_district . ', Police Station = ' . $filter_ps. ', Start Date = ' . $filter_entry_start_date. ', End Date = ' . $filter_entry_end_date];
+    $filtered_with = ['Participant ID = ' . $filter_id . ', Name = ' . $filter_name . ', NID = ' . $filter_nid . ', Passport = ' . $filter_passport . ', Division = ' . $filter_division . ', District = ' . $filter_district . ', Sub-District = ' . $filter_sub_district . ', Police Station = ' . $filter_ps . ', Start Date = ' . $filter_entry_start_date . ', End Date = ' . $filter_entry_end_date];
     $rowFromVal = WriterEntityFactory::createRowFromArray($filtered_with);
     $writer->addRow($rowFromVal);
 
@@ -137,33 +219,31 @@ if ($_GET['download_excel']) {
     $writer->addRow($rowFromVal);
 
     $header = [
-                "SL",
-                'Participant ID', 
-                'Full Name', 
-                "NID Number", 
-                "Birth Registration Number",
-                'Date of Birth', 
-                'Gender', 
-                'Mobile No', 
-                'Emergency Mobile No',
-                'Name of that Person', 
-                "Relation with Participant", 
-                "Father's Name",
-                "Mother's Name",
-                "Village",
-                'Ward No', 
-                'Union',
-                'Upazilla',
-                "District", 
-                'Present Address of Beneficiary', 
-
-                'Support Date',
-                'Arrival Place',
-                "Immediate support services received", 
-
+        "SL",
+        'Participant ID',
+        'Full Name',
+        "NID Number",
+        "Birth Registration Number",
+        'Date of Birth',
+        'Gender',
+        'Mobile No',
+        'Emergency Mobile No',
+        'Name of that Person',
+        "Relation with Participant",
+        "Father's Name",
+        "Mother's Name",
+        "Village",
+        'Ward No',
+        'Union',
+        'Upazilla',
+        "District",
+        'Present Address of Beneficiary',
+        'Support Date',
+        'Arrival Place',
+        "Immediate support services received",
     ];
 
-    $rowFromVal = WriterEntityFactory::createRowFromArray($header,$style);
+    $rowFromVal = WriterEntityFactory::createRowFromArray($header, $style);
     $writer->addRow($rowFromVal);
     $multipleRows = array();
 
@@ -200,13 +280,12 @@ if ($_GET['download_excel']) {
             ];
 
             $multipleRows[] = WriterEntityFactory::createRow($cells);
-
         }
     }
-    $writer->addRows($multipleRows); 
+    $writer->addRows($multipleRows);
 
     $currentSheet = $writer->getCurrentSheet();
-    $mergeRanges = ['A1:V1','A2:V2','A3:V3']; // you can list the cells you want to merge like this ['A1:A4','A1:E1']
+    $mergeRanges = ['A1:V1', 'A2:V2', 'A3:V3']; // you can list the cells you want to merge like this ['A1:A4','A1:E1']
     $currentSheet->setMergeRanges($mergeRanges);
 
     $writer->close();
@@ -220,16 +299,16 @@ doAction('render_start');
     <h1>All Cases</h1>
     <div class="oh">
         <div class="btn-group btn-group-sm">
-            <?php
-            echo linkButtonGenerator(array(
-                'href' => '?download_excel=1&customer_id=' . $filter_customer_id . '&name=' . $filter_name . '&nid=' . $filter_nid . '&birth=' . $filter_birth . '&division=' . $filter_division . '&district=' . $filter_district . '&sub_district=' . $filter_sub_district . '&entry_start_date=' . $filter_entry_start_date . '&entry_end_date=' . $filter_entry_end_date,
-                'attributes' => array('target' => '_blank'),
-                'action' => 'download',
-                'icon' => 'icon_download',
-                'text' => 'Download Case',
-                'title' => 'Download Case',
-            ));
-            ?>
+<?php
+echo linkButtonGenerator(array(
+    'href' => '?download_excel=1&customer_id=' . $filter_customer_id . '&name=' . $filter_name . '&nid=' . $filter_nid . '&birth=' . $filter_birth . '&division=' . $filter_division . '&district=' . $filter_district . '&sub_district=' . $filter_sub_district . '&entry_start_date=' . $filter_entry_start_date . '&entry_end_date=' . $filter_entry_end_date,
+    'attributes' => array('target' => '_blank'),
+    'action' => 'download',
+    'icon' => 'icon_download',
+    'text' => 'Download Case',
+    'title' => 'Download Case',
+));
+?>
         </div>
     </div>
 </div>
@@ -254,7 +333,7 @@ echo formProcessor::form_elements('birth', 'birth', array(
     <label>Division</label>
     <div class="select2-primary">
         <select class="form-control division" name="division" style="text-transform: capitalize">
-            <?php if ($filter_division) : ?>
+<?php if ($filter_division) : ?>
                 <option value="<?php echo $filter_division ?>"><?php echo $filter_division ?></option>
             <?php else: ?>
                 <option value="">Select One</option>
@@ -269,7 +348,7 @@ echo formProcessor::form_elements('birth', 'birth', array(
     <label>District</label>
     <div class="select2-primary">
         <select class="form-control district" name="district" id="districtList" style="text-transform: capitalize">
-            <?php if ($filter_district) : ?>
+<?php if ($filter_district) : ?>
                 <option value="<?php echo $filter_district ?>"><?php echo $filter_district ?></option>
             <?php endif ?>
         </select>
@@ -279,7 +358,7 @@ echo formProcessor::form_elements('birth', 'birth', array(
     <label>Upazila</label>
     <div class="select2-primary">
         <select class="form-control subdistrict" name="sub_district" id="subdistrictList" style="text-transform: capitalize">
-            <?php if ($filter_sub_district) : ?>
+<?php if ($filter_sub_district) : ?>
                 <option value="<?php echo $filter_sub_district ?>"><?php echo $filter_sub_district ?></option>
             <?php endif ?>
         </select>
@@ -312,13 +391,13 @@ $filterForm = ob_get_clean();
 filterForm($filterForm);
 ?>
 <div class="table-primary table-responsive">
-    <?php if ($filterString): ?>
+<?php if ($filterString): ?>
         <div class="table-header">
             Filtered With: <?php echo implode(', ', $filterString) ?>
         </div>
-    <?php endif; ?>
+<?php endif; ?>
     <div class="table-header">
-        <?php echo searchResultText($cases['total'], $start, $per_page_items, count($cases['data']), 'cases') ?>
+    <?php echo searchResultText($cases['total'], $start, $per_page_items, count($cases['data']), 'cases') ?>
     </div>
     <table class="table table-bordered table-condensed">
         <thead>
@@ -333,9 +412,9 @@ filterForm($filterForm);
             </tr>
         </thead>
         <tbody>
-            <?php
-            foreach ($cases['data'] as $i => $case) {
-                ?>
+<?php
+foreach ($cases['data'] as $i => $case) {
+    ?>
                 <tr>
                     <td><?php echo $case['customer_id']; ?></td>
                     <td><?php echo $case['full_name']; ?></td>
@@ -344,17 +423,17 @@ filterForm($filterForm);
                     <td style="text-transform: capitalize"><?php echo '<b>Division - </b>' . $case['permanent_division'] . ',<br><b>District - </b>' . $case['permanent_district'] . ',<br><b>Upazila - </b>' . $case['permanent_sub_district'] ?></td>
                     <td style="text-transform: capitalize"><?php echo $case['customer_status']; ?></td>
                     <td class="tar action_column">
-                        <?php if (has_permission('edit_case')): ?>
+    <?php if (has_permission('edit_case')): ?>
                             <div class="btn-group btn-group-sm">
-                                <?php
-                                echo linkButtonGenerator(array(
-                                    'href' => build_url(array('action' => 'add_edit_case', 'edit' => $case['fk_customer_id'])),
-                                    'action' => 'edit',
-                                    'icon' => 'icon_edit',
-                                    'text' => 'Edit',
-                                    'title' => 'Edit Case',
-                                ));
-                                ?>
+                            <?php
+                            echo linkButtonGenerator(array(
+                                'href' => build_url(array('action' => 'add_edit_case', 'edit' => $case['fk_customer_id'])),
+                                'action' => 'edit',
+                                'icon' => 'icon_edit',
+                                'text' => 'Edit',
+                                'title' => 'Edit Case',
+                            ));
+                            ?>
                             </div>
                             <div class="btn-group btn-group-sm">
                                 <?php
@@ -367,17 +446,17 @@ filterForm($filterForm);
                                 ));
                                 ?>
                             </div>
-                        <?php endif; ?>
+                            <?php endif; ?>
                     </td>
                 </tr>
-                <?php
-            }
-            ?>
+                        <?php
+                    }
+                    ?>
         </tbody>
     </table>
     <div class="table-footer oh">
         <div class="pull-left">
-            <?php echo $pagination ?>
+<?php echo $pagination ?>
         </div>
     </div>
 </div>
