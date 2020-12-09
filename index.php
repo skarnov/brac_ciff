8bb3dd95db7c2bae215f2733d05d05e9214980ad
3_devs_it <motaleb@3-devs.com>
2020-12-09 16:14:52 +0600

    Participant Profile, Case Management, Event excel export

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 error_reporting(0);
+include('vendor/autoload.php');
 include('system/kick_loads.php');
--- site/jacks/optional/event_management/pages/list_events.php
+++ site/jacks/optional/event_management/pages/list_events.php
@@ -1,4 +1,10 @@
 <?php
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Common\Entity\Style\Color;
+use Box\Spout\Common\Entity\Style\CellAlignment;
+
 $start = $_GET['start'] ? $_GET['start'] : 0;
 $per_page_items = 10;
 
@@ -42,6 +48,195 @@ if ($filter_entry_start_date)
 if ($filter_entry_end_date)
     $filterString[] = 'End Date: ' . $filter_entry_end_date;
 
+if ($_GET['download_excel']) {
+    unset($args['limit']);
+    $data = $this->get_events($args);
+    $data = $data['data'];
+
+    // This will be here in our project
+
+    $writer =WriterEntityFactory::createXLSXWriter();
+    $style = (new StyleBuilder())
+           ->setFontBold()
+           ->setFontSize(12)
+           //->setShouldWrapText()
+           ->build();
+
+    $fileName = 'event-management-' . time() . '.xlsx';
+    $writer->openToBrowser($fileName); // stream data directly to the browser
+
+    // Header text
+    $style2 = (new StyleBuilder())
+           ->setFontBold()
+           ->setFontSize(15)
+           //->setFontColor(Color::BLUE)
+           ->setShouldWrapText()
+           ->setCellAlignment(CellAlignment::LEFT)
+           ->build();
+
+    /** add a row at a time */
+    $report_head = ['Event Management Report '];
+    $singleRow = WriterEntityFactory::createRowFromArray($report_head,$style2);
+    $writer->addRow($singleRow);
+
+    $report_date = ['Date: '.Date('d-m-Y H:i')];
+    $reportDateRow = WriterEntityFactory::createRowFromArray($report_date);
+    $writer->addRow($reportDateRow);
+
+    $filtered_with = ['Division = ' . $filter_division . ', District = ' . $filter_district];
+    $rowFromVal = WriterEntityFactory::createRowFromArray($filtered_with);
+    $writer->addRow($rowFromVal);
+
+    $empty_row = [''];
+    $rowFromVal = WriterEntityFactory::createRowFromArray($empty_row);
+    $writer->addRow($rowFromVal);
+
+    $header = [
+                "SL",
+                'Event Name', 
+                'Start Date', 
+                'Start Time',
+                "End Date", 
+                "End Time", 
+                "Division",
+                "District",
+                'Upazila', 
+                'Union', 
+                'Event Location', 
+                'Event Village',
+                'Event Ward',
+                'Submitted By', 
+                'Participant Number', 
+                'Event Validation Count',
+                'Observation Score', 
+                'Preparatory Work', 
+                'Time management of the event was', 
+                'Participants attention', 
+                'Logistical arrangements', 
+                'Relevancy of delivery of messages', 
+                'Participants Feedback', 
+                'Event Note', 
+
+    ];
+
+    $rowFromVal = WriterEntityFactory::createRowFromArray($header,$style);
+    $writer->addRow($rowFromVal);
+    $multipleRows = array();
+
+    if ($data) {
+        $count = 0;
+        foreach ($data as $i => $event) {
+            $nid_number = $case_info['nid_number'] ? $case_info['nid_number'] : 'N/A';
+            $birth_reg_number = $case_info['birth_reg_number'] ? $case_info['birth_reg_number'] : 'N/A';
+            $support_date = $case_info['entry_date'] ? date('d-m-Y', strtotime($case_info['entry_date'])) : 'N/A';
+            $itme_aanagement = null;
+            if($event['time_management']==1):
+                $itme_aanagement = 'Not Observed';
+            elseif($event['time_management']==2):
+                $itme_aanagement = 'Need To Improved';
+            elseif($event['time_management']==3):
+                $itme_aanagement = 'Neutral';
+            elseif($event['time_management']==4):
+                $itme_aanagement = 'Good';
+            elseif($event['time_management']==5):
+                $itme_aanagement = 'Excellent';
+            endif;
+
+            $participants_attention = null;
+            if($event['participants_attention']==1):
+                $participants_attention = 'Not Observed';
+            elseif($event['participants_attention']==2):
+                $participants_attention = 'Need To Improved';
+            elseif($event['participants_attention']==3):
+                $participants_attention = 'Neutral';
+            elseif($event['participants_attention']==4):
+                $participants_attention = 'Good';
+            elseif($event['participants_attention']==5):
+                $participants_attention = 'Excellent';
+            endif;
+
+            $logistical_arrangements = null;
+            if($event['logistical_arrangements']==1):
+                $logistical_arrangements = 'Not Observed';
+            elseif($event['logistical_arrangements']==2):
+                $logistical_arrangements = 'Need To Improved';
+            elseif($event['logistical_arrangements']==3):
+                $logistical_arrangements = 'Neutral';
+            elseif($event['logistical_arrangements']==4):
+                $logistical_arrangements = 'Good';
+            elseif($event['participants_attention']==5):
+                $logistical_arrangements = 'Excellent';
+            endif;
+
+            $relevancy_delivery = null;
+            if($event['relevancy_delivery']==1):
+                $relevancy_delivery = 'Not Observed';
+            elseif($event['relevancy_delivery']==2):
+                $relevancy_delivery = 'Need To Improved';
+            elseif($event['relevancy_delivery']==3):
+                $relevancy_delivery = 'Neutral';
+            elseif($event['relevancy_delivery']==4):
+                $relevancy_delivery = 'Good';
+            elseif($event['relevancy_delivery']==5):
+                $relevancy_delivery = 'Excellent';
+            endif;
+
+            $participants_feedback = null;
+            if($event['participants_feedback']==1):
+                $participants_feedback = 'Not Observed';
+            elseif($event['participants_feedback']==2):
+                $participants_feedback = 'Need To Improved';
+            elseif($event['participants_feedback']==3):
+                $participants_feedback = 'Neutral';
+            elseif($event['participants_feedback']==4):
+                $participants_feedback = 'Good';
+            elseif($event['participants_feedback']==5):
+                $participants_feedback = 'Excellent';
+            endif;
+            
+
+            $cells = [
+                WriterEntityFactory::createCell(++$count),
+                WriterEntityFactory::createCell($event['activity_name']),
+                WriterEntityFactory::createCell(date('d-m-Y', strtotime($event['event_start_date']))),
+                WriterEntityFactory::createCell(date('H:i', strtotime($event['event_start_time']))),
+                WriterEntityFactory::createCell(date('d-m-Y', strtotime($event['event_end_date']))),
+                WriterEntityFactory::createCell(date('H:i', strtotime($event['event_end_time']))),
+                WriterEntityFactory::createCell($event['event_division']),
+                WriterEntityFactory::createCell($event['event_district']),
+                WriterEntityFactory::createCell($event['event_upazila']),
+                WriterEntityFactory::createCell($event['event_union']),
+                WriterEntityFactory::createCell($event['event_location']),
+                WriterEntityFactory::createCell($event['event_village']),
+                WriterEntityFactory::createCell($event['event_ward']),
+
+                WriterEntityFactory::createCell($event['user_fullname']),
+                WriterEntityFactory::createCell('Boy: ' . $event['participant_boy'] . '; Girl: ' . $event['participant_girl'] . '; Men: ' . $event['participant_male'] . '; Women: ' . $event['participant_female']),
+                WriterEntityFactory::createCell($event['validation_count']),
+                WriterEntityFactory::createCell($event['observation_score']),
+                WriterEntityFactory::createCell($event['preparatory_work']),
+                WriterEntityFactory::createCell($itme_aanagement),
+                WriterEntityFactory::createCell($participants_attention),
+                WriterEntityFactory::createCell($logistical_arrangements),
+                WriterEntityFactory::createCell($relevancy_delivery),
+                WriterEntityFactory::createCell($participants_feedback),
+                WriterEntityFactory::createCell($event_note),
+            ];
+
+            $multipleRows[] = WriterEntityFactory::createRow($cells);
+
+        }
+    }
+    $writer->addRows($multipleRows); 
+
+    $currentSheet = $writer->getCurrentSheet();
+    $mergeRanges = ['A1:X1','A2:X2','A3:X3']; // you can list the cells you want to merge like this ['A1:A4','A1:E1']
+    $currentSheet->setMergeRanges($mergeRanges);
+
+    $writer->close();
+    exit;
+    // End this is to our project
+}
 
 doAction('render_start');
 ?>
@@ -58,6 +253,19 @@ doAction('render_start');
                         'icon' => 'icon_add',
                         'text' => 'Add Event',
                         'title' => 'Add Event',
+                    ));
+                    ?>
+                </div>
+
+                <div class="btn-group btn-group-sm">
+                    <?php
+                    echo linkButtonGenerator(array(
+                        'href' => '?download_excel=1&division=' . $filter_division . '&district=' . $filter_district,
+                        'attributes' => array('target' => '_blank'),
+                        'action' => 'download',
+                        'icon' => 'icon_download',
+                        'text' => 'Download Case',
+                        'title' => 'Download Case',
                     ));
                     ?>
                 </div>
